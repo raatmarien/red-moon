262bdf04fa4ce9798c19593106f177f899eb135d
Stephen Michel <git@smichel.me>
2020-03-11 12:55:23 -0400

    Clean up AboutFragment
    Remove unnecessary safe call and stop overusing scoping functions

--- app/src/main/java/com/jmstudios/redmoon/AboutFragment.kt
+++ app/src/main/java/com/jmstudios/redmoon/AboutFragment.kt
@@ -6,7 +6,6 @@
 package com.jmstudios.redmoon
 
 import android.os.Bundle
-import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 import com.jmstudios.redmoon.util.*
@@ -14,12 +13,12 @@ import com.jmstudios.redmoon.util.*
 class AboutFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.about, rootKey)
-        pref(R.string.pref_key_version)?.apply{
-            summary = BuildConfig.VERSION_NAME
-            Preference.OnPreferenceClickListener {
+        pref(R.string.pref_key_version)?.let { versionPref ->
+            versionPref.summary = BuildConfig.VERSION_NAME
+            versionPref.setOnPreferenceClickListener { _ ->
                 activity?.let { showChangelog(it) }
                 true
-            }?.let { onPreferenceClickListener = it }
+            }
         }
     }
     companion object : Logger()
